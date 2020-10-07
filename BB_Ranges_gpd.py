85efb52a6e3d417315b40b04a954da9d45d4a163
WenkChr <christopher.wenkoff@canada.ca>
2020-10-07 13:51:26 -0400

    Added range sorting code

--- BB_Ranges_gpd.py
+++ BB_Ranges_gpd.py
@@ -15,6 +15,23 @@ def records(filename, usecols, **kwargs):
             f['properties'] = {k: feature['properties'][k] for k in usecols}
             yield f
 
+def RangeCompareMatcher(AF_VAL, AT_VAL, OA_MAX, OA_MIN, side): 
+    # Compares the OA range and the NGD range in matches and return a set class value for easy comparison    
+    vals =[AF_VAL, AT_VAL, OA_MIN, OA_MAX]
+    if AF_VAL > AT_VAL:
+        # Then AF pairs with min and AT pairs with max
+        vals = [AT_VAL, AF_VAL, OA_MIN, OA_MAX]
+    NGD_Range = int(vals[0]) - int(vals[1])
+    OA_Range = int(vals[2]) - int(vals[3])
+    if vals[0] == OA_MIN and vals[1] == OA_MAX:
+        return 'EQUAL'
+    if vals[0] > OA_MIN and vals[1] < OA_MAX:
+        return 'NGD RANGE INSIDE OA RANGE'
+    if OA_MIN > vals[0] and OA_MAX < vals[1]:
+        return 'OA RANGE INSIDE NGD RANGE'
+    if (OA_MIN == vals[0] and OA_MAX != vals[1]) or (OA_MIN != vals[0] and OA_MAX == vals[1]):
+        return 'ONE VALUE MATCH'
+    else: return 'RANGES OFFSET'
 #-------------------------------------------------------------------------------------------------------------------------
 # Inputs
 
@@ -101,6 +118,82 @@ for UID in points_sjoin['BB_UID'].unique().tolist():
                                                 street_df.NUMBER.min(), 
                                                 street]
 out_ranges_df['S_NAME_ONLY'] = out_ranges_df.Street_Name.str.split().str.get(0)
-
-print('DONE!')
+# Compare BB ranges to the NGD A ranges
+for side in ['L', 'R']:
+    # Side outputs
+    out_df_columns = ['NGD_UID','BB_UID', 'Max_Address', 'Min_Address', 'OA_Street_Name', f'NGD_STR_ID_{side}', f'AF{side}_VAL', f'AF{side}_SRC', f'AT{side}_VAL', f'AT{side}_SRC', f'STR_LABEL_NME_{side}', 'Match_Type']
+    out_df_rows = []
+    rejects_rows = []
+    output_counts = {'FULL': 0, 'PARTIAL' : 0, 'RATIO' : 0}
+    print(f'Length of {side}: {len(NGD_AL_gdf)}')
+    # Loop over the NGD_AL and find matches with BB ranges
+    print(f'Comparing ranges on {side} side to the NGD_AL')
+    for Nrow in NGD_AL_gdf.itertuples():
+        # compare df's on the street name. If this returns a match add to the out df
+        bbuid = NGD_AL_gdf.iloc[Nrow.Index]
+        bbuid = bbuid[f'BB_UID_{side}']
+        # Perfect street name match check
+        UID_ranges_df = out_ranges_df.loc[(out_ranges_df['BB_UID'] == bbuid) & (out_ranges_df['Street_Name'] == Nrow.STR_LABEL_NME)]
+        if len(UID_ranges_df) > 0: # If there is a matching record add this to the out df
+            row = [Nrow.NGD_UID, #NGD_UID
+                        bbuid, #BB_UID        
+                        UID_ranges_df.iloc[0]['Max_Address'], # Max_Address
+                        UID_ranges_df.iloc[0]['Min_Address'], # Min_Address
+                        UID_ranges_df.iloc[0]['Street_Name'], # OA_Street_Name
+                        NGD_AL_gdf.iloc[Nrow.Index][f'NGD_STR_UID_{side}'], # NGD_STR_UID
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AF{side}_VAL'], # AF VAL
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AF{side}_SRC'], # AF SRC
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AT{side}_VAL'], # AT Val
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AT{side}_SRC'], # AT SRC
+                        Nrow.STR_LABEL_NME, # STR_LABEL_NME 
+                        'FULL'] # Street Name Match Type                    
+            out_df_rows.append(row)
+            output_counts['FULL'] += 1
+            continue
+        # Partial Street Match check
+        UID_ranges_df = out_ranges_df.loc[(out_ranges_df['BB_UID'] == bbuid) & (out_ranges_df['S_NAME_ONLY'] == Nrow.STR_LABEL_NME.split(' ')[0])]
+        if len(UID_ranges_df) > 0:
+            row = [Nrow.NGD_UID, #NGD_UID
+                        bbuid, #BB_UID        
+                        UID_ranges_df.iloc[0]['Max_Address'], # Max_Address
+                        UID_ranges_df.iloc[0]['Min_Address'], # Min_Address
+                        UID_ranges_df.iloc[0]['Street_Name'], # OA_Street_Name
+                        NGD_AL_gdf.iloc[Nrow.Index][f'NGD_STR_UID_{side}'], # NGD_STR_UID
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AF{side}_VAL'], # AF VAL
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AF{side}_SRC'], # AF SRC
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AT{side}_VAL'], # AT Val
+                        NGD_AL_gdf.iloc[Nrow.Index][f'AT{side}_SRC'], # AT SRC
+                        Nrow.STR_LABEL_NME, # STR_LABEL_NME 
+                        'PARTIAL'] # Street name match type 
+            out_df_rows.append(row)
+            output_counts['PARTIAL'] += 1
+            continue
+        # if you get to this point then these records don't have a match via the current methods
+        UID_ranges_df = out_ranges_df.loc[(out_ranges_df['BB_UID'] == bbuid)]
+        if len(UID_ranges_df) > 0:
+            for r in UID_ranges_df.itertuples(): 
+                row = [Nrow.NGD_UID, #NGD_UID
+                            bbuid, #BB_UID        
+                            r.Max_Address, # Max_Address
+                            r.Min_Address, # Min_Address
+                            r.Street_Name, # OA_Street_Name
+                            NGD_AL_gdf.iloc[Nrow.Index][f'NGD_STR_UID_{side}'], # NGD_STR_UID
+                            NGD_AL_gdf.iloc[Nrow.Index][f'AF{side}_VAL'], # AF VAL
+                            NGD_AL_gdf.iloc[Nrow.Index][f'AF{side}_SRC'], # AF SRC
+                            NGD_AL_gdf.iloc[Nrow.Index][f'AT{side}_VAL'], # AT Val
+                            NGD_AL_gdf.iloc[Nrow.Index][f'AT{side}_SRC'], # AT SRC
+                            Nrow.STR_LABEL_NME, # STR_LABEL_NME 
+                            ] 
+            rejects_rows.append(row)
+    print(f"Final output counts for {side}. FULL: {output_counts['FULL']} PARTIAL: {output_counts['PARTIAL']} RATIO: {output_counts['RATIO']}")
+    out_df = pd.DataFrame(out_df_rows, columns= out_df_columns)
+    reject_df = pd.DataFrame(rejects_rows,  columns= out_df_columns[:len(out_df_columns)-1])
+    print(f"Number of rows to manually recheck: {len(reject_df['NGD_UID'].unique().tolist())}")
+    out_df[f'AF{side}_VAL'] = out_df[f'AF{side}_VAL'].fillna(0)
+    out_df[f'AT{side}_VAL'] = out_df[f'AT{side}_VAL'].fillna(0)
+    # Calculate match type var
+    out_df['Range_Match_Type'] =  out_df.apply(lambda row: RangeCompareMatcher(row[f'AF{side}_VAL'], row[f'AT{side}_VAL'], row.Max_Address, row.Min_Address, side), axis=1)
+    out_df.to_csv(os.path.join(r'H:\NGD_A_Complete_Ranges', f'testBB_{side}.csv'), index= False)
+    reject_df.to_csv(os.path.join(r'H:\NGD_A_Complete_Ranges', f'testBB_{side}_rejects.csv'), index= False)
+print('DONE!')   
                 
