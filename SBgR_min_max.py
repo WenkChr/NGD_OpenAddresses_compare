625856f2c3e8a6bab7979d61d88cb4c2ca2c1fa0
WenkChr <christopher.wenkoff@canada.ca>
2020-10-15 07:44:18 -0400

    Creation of merger script to combine both the OA matching and the SBgR matching

--- SBgR_min_max.py
+++ SBgR_min_max.py
@@ -26,6 +26,7 @@ def RangeCompareMatcher(AF_VAL, AT_VAL, OA_MAX, OA_MIN, side):
 
 #----------------------------------------------------------------------------------------------------------
 #Inputs
+workingPath = r'H:\NGD_OpenAddresses_compare'
 workingGDB = r'H:\NGD_OpenAddresses_compare\Working.gdb'
 SBgR_ranges = r'H:\NGD_OpenAddresses_compare\vancouver_min_max_sbgr.csv'
 NGD_AL = os.path.join(workingGDB, 'NGD_AL_clipped')
@@ -45,7 +46,7 @@ SBgR_df['ST_NAME'] = SBgR_df['ST_NAME'].str.upper()
 #Loop over sides to get ranges
 for side in ['L', 'R']:
     # Side outputs
-    out_df_columns = ['NGD_UID','BF_UID', 'MIN', 'MAX', 'SBgR_Street_Name', f'NGD_STR_ID_{side}', f'AF{side}_VAL', f'AF{side}_SRC', f'AT{side}_VAL', f'AT{side}_SRC', f'STR_NME{side}', f'STR_TYP_{side}']
+    out_df_columns = ['NGD_UID','BF_UID', 'SBgR_MIN', 'SBgR_MAX', 'SBgR_Street_Name', 'SBgR_ST_TYPE', f'NGD_STR_ID_{side}', f'AF{side}_VAL', f'AF{side}_SRC', f'AT{side}_VAL', f'AT{side}_SRC', f'STR_LABEL_NME_{side}']
     out_df_rows = []
     rejects_rows = []
     # NGD_AL DF setup and selection vars
@@ -70,31 +71,33 @@ for side in ['L', 'R']:
                         UID_ranges_df.iloc[0]['MIN'], # MIN        
                         UID_ranges_df.iloc[0]['MAX'], # MAX
                         UID_ranges_df.iloc[0]['ST_NAME'], # Street_Name
+                        UID_ranges_df.iloc[0]['ST_TYPE'],
                         NGD_AL_df.iloc[Nrow.Index][f'NGD_STR_UID_{side}'], # NGD_STR_UID
                         NGD_AL_df.iloc[Nrow.Index][f'AF{side}_VAL'], # AF VAL
                         NGD_AL_df.iloc[Nrow.Index][f'AF{side}_SRC'], # AF SRC
                         NGD_AL_df.iloc[Nrow.Index][f'AT{side}_VAL'], # AT Val
                         NGD_AL_df.iloc[Nrow.Index][f'AT{side}_SRC'], # AT SRC
-                        Nrow.STR_NME, # STR_LABEL_NME 
-                        Nrow.STR_TYP]
+                        Nrow.STR_LABEL_NME, # STR_LABEL_NME 
+                        ]
             out_df_rows.append(row)
             continue
         # if you get to this point then these records don't have a match via the current methods
         UID_ranges_df = SBgR_df.loc[(SBgR_df['BF_SDI_BF_UID'] == bfuid)]
         if len(UID_ranges_df) > 0:
             for r in UID_ranges_df.itertuples(): 
-                row = [NGD_AL_df.iloc[Nrow.Index][f'BF_UID_{side}'], #NGD_UID
+                row = [NGD_AL_df.iloc[Nrow.Index],[f'BF_UID_{side}'], #NGD_UID
                             bfuid, #BF_UID        
-                            r.MIN, # MIN
+                            r.MIN, # MIN                      
                             r.MAX, # MAX
-                            r.ST_NAME, # OA_Street_Name
+                            r.ST_NAME, # Street_Name
+                            r.ST_TYPE,
                             NGD_AL_df.iloc[Nrow.Index][f'NGD_STR_UID_{side}'], # NGD_STR_UID
                             NGD_AL_df.iloc[Nrow.Index][f'AF{side}_VAL'], # AF VAL
                             NGD_AL_df.iloc[Nrow.Index][f'AF{side}_SRC'], # AF SRC
                             NGD_AL_df.iloc[Nrow.Index][f'AT{side}_VAL'], # AT Val
                             NGD_AL_df.iloc[Nrow.Index][f'AT{side}_SRC'], # AT SRC
-                            Nrow.STR_NME, # STR_LABEL_NME 
-                            Nrow.STR_TYP] 
+                            Nrow.STR_LABEL_NME, # STR_LABEL_NME 
+                            ] 
             rejects_rows.append(row)
     print(f"Final output count for {side}: Matches- {len(out_df_rows)} Rejects- {len(rejects_rows)}")
     out_df = pd.DataFrame(out_df_rows, columns= out_df_columns)
@@ -103,8 +106,40 @@ for side in ['L', 'R']:
     out_df[f'AF{side}_VAL'] = out_df[f'AF{side}_VAL'].fillna(0)
     out_df[f'AT{side}_VAL'] = out_df[f'AT{side}_VAL'].fillna(0)
     # Calculate match type var
-    out_df['Range_Match_Type'] =  out_df.apply(lambda row: RangeCompareMatcher(row[f'AF{side}_VAL'], row[f'AT{side}_VAL'], row['MAX'], row['MIN'], side), axis=1)
+    out_df['SBgR_Range_Match_Type'] =  out_df.apply(lambda row: RangeCompareMatcher(row[f'AF{side}_VAL'], row[f'AT{side}_VAL'], row['SBgR_MAX'], row['SBgR_MIN'], side), axis=1)
     out_df.to_csv(os.path.join(r'H:\NGD_OpenAddresses_compare', f'testSBgR_{side}.csv'), index= False)
     reject_df.to_csv(os.path.join(r'H:\NGD_OpenAddresses_compare', f'testSBgR_{side}_rejects.csv'), index= False)
 
+    #Combine the OA output and the SBgR output
+    oa_df = pd.read_csv(os.path.join(workingPath, f'testOABB_{side}.csv'))
+    # Prep imported data from OA matching
+    oa_df.columns = ['OA_' + str(col) for col in oa_df.columns]
+    oa_df.rename(columns= {'OA_NGD_UID' : 'NGD_UID', 'OA_BB_UID': 'BB_UID'}, inplace= True)
+    #Merge imported and cleaned data
+    merged = pd.merge(out_df, oa_df, on='NGD_UID', how= 'outer')
+    for col in [f'NGD_STR_ID_{side}', f'AF{side}_VAL', f'AF{side}_SRC', f'AT{side}_VAL', f'AT{side}_SRC', f'STR_LABEL_NME_{side}']:
+        merged[col] = merged[col].fillna(merged['OA_' + col])
+    out_cols = ['NGD_UID', 
+            'BF_UID',
+            'BB_UID',
+            f'NGD_STR_ID_{side}', 
+            f'AF{side}_VAL', 
+            f'AF{side}_SRC', 
+            f'AT{side}_VAL', 
+            f'AT{side}_SRC', 
+            f'STR_LABEL_NME_{side}', 
+            'SBgR_MIN', 
+            'SBgR_MAX', 
+            'SBgR_Street_Name',
+            'SBgR_ST_TYPE', 
+            'SBgR_Range_Match_Type', 
+            'OA_Min_Address',
+            'OA_Max_Address', 
+            'OA_OA_Street_Name', 
+            f'OA_STR_LABEL_NME_{side}', 
+            'OA_Match_Type', 
+            'OA_Range_Match_Type', 
+            ]
+    merged = merged[out_cols]
+    merged.to_csv(os.path.join(r'H:\NGD_OpenAddresses_compare', f'Merged_OA_SBgR_{side}.csv'), index= False)
 print('DONE!')
